ff5f64d6a1901699a0f39f6c17535f188dcccfb6
Matt Pindell <pindell.m@gmail.com>
2016-04-18 20:09:54 -0600

    Add Volunteer description and hours to Volunteer Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+1.upto(10) do |i|
+  Task.create(name:        "Name #{i}",
+              description: "Description #{i}",
+              date:        Date.new,
+              start_time:  Time.new,
+              hours:       "#{i}",
+              image_path:  "https://robohash.org/#{i}")
+end
--- spec/features/vistior_can_view_all_tasks_spec.rb
+++ spec/features/vistior_can_view_all_tasks_spec.rb
@@ -7,12 +7,18 @@ RSpec.feature "Visitor can view all tasks" do
     visit tasks_path
 
     expect(page).to have_content tasks[0].name
+    expect(page).to have_content tasks[0].description
+    expect(page).to have_content tasks[0].hours
     expect(page).to have_css("img[src*='#{tasks[0].image_path}']")
 
     expect(page).to have_content tasks[1].name
+    expect(page).to have_content tasks[1].description
+    expect(page).to have_content tasks[1].hours
     expect(page).to have_css("img[src*='#{tasks[1].image_path}']")
 
     expect(page).to have_content tasks[2].name
+    expect(page).to have_content tasks[2].description
+    expect(page).to have_content tasks[2].hours
     expect(page).to have_css("img[src*='#{tasks[2].image_path}']")
   end
 
