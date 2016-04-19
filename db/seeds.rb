36848e919acce3570e4087de5a72ca583674554d
Matt Pindell <pindell.m@gmail.com>
2016-04-18 21:53:36 -0600

    Add two Cities to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
+city_one = City.create(name: "Denver",
+                       state: "CO")
 
-1.upto(10) do |i|
+city_two = City.create(name: "Aurora",
+                       state: "CO")
+
+
+1.upto(5) do |i|
   Task.create(name:        "Name #{i}",
               description: "Description #{i}",
               date:        Date.new,
               start_time:  Time.new,
               hours:       "#{i}",
-              image_path:  "https://robohash.org/#{i}")
+              image_path:  "https://robohash.org/#{i}",
+              city_id:     city_one.id)
+end
+
+1.upto(5) do |i|
+  Task.create(name:        "Name #{i}",
+              description: "Description #{i}",
+              date:        Date.new,
+              start_time:  Time.new,
+              hours:       "#{i}",
+              image_path:  "https://robohash.org/#{i}",
+              city_id:     city_two.id)
 end
