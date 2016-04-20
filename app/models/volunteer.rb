aa6944b2f068c4c7e77cdf1b40e06393b46259bf
Matt Pindell <pindell.m@gmail.com>
2016-04-20 17:40:35 -0600

    Add Volunteer model test

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,8 @@
 class Volunteer < ActiveRecord::Base
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
