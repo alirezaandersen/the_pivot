d01c654b013b62891b9921574c1abc0bad52a4cf
lingtran <lxiutran@gmail.com>
2016-04-20 14:26:26 -0600

    WIP Add landing page feature
    Pass test for baseline of landing page feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
+  root to: 'home#index'
   resources :tasks, only: [:index]
   resources :cart_tasks, only: [:create, :destroy]
   get "/cart", to: 'cart_tasks#show'
   get "/:name", to: 'cities#show', as: :city
-  # resources :cities, only:[:show], param: :name, as
 end
---
+++ spec/features/visitor_can_view_landing_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can view landing page" do
+  scenario "they can navigate to the tasks index" do
+    cities = create_list(:city_with_tasks, 4)
+
+    visit root_path
+
+    expect(page).to have_button "View All Tasks"
+    cities.each do |city|
+      expect(page).to have_content city.name
+    end
+
+    click_button "View All Tasks"
+
+    expect(page).to have_current_path tasks_path
+    cities.each do |city|
+      city.tasks.each do |task|
+        expect(page).to have_content task.name
+        expect(page).to have_content task.description
+        expect(page).to have_content task.hours
+        expect(page).to have_css "img[src*='#{task.image_path}']"
+      end
+    end
+  end
+end
