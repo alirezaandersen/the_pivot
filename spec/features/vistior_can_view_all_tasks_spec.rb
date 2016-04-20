4d17eb39723609ddafc8fa54950e387491470da0
patrickwhardy <patrickwhardy@gmail.com>
2016-04-19 18:17:19 -0600

    Test tasks from other cities do not appear on city#show

--- spec/features/vistior_can_view_all_tasks_spec.rb
+++ spec/features/vistior_can_view_all_tasks_spec.rb
@@ -26,5 +26,17 @@ RSpec.feature "Visitor can view all tasks" do
       expect(page).to have_content task.hours
       expect(page).to have_css("img[src*='#{task.image_path}']")
     end
+
+  end
+
+  scenario "they cannot see tasks that belong to another city" do
+    city1, city2 = create_list(:city_with_tasks, 2)
+
+    visit city_path(city1.name)
+
+    city2.tasks.each do |task|
+      expect(page).not_to have_content task.name
+    end
+
   end
 end
