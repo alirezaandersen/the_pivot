faeb94ffe8f5a9be1c11d8ab4dd3fcc48406d94c
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-04-22 17:56:40 -0600

    Add view retired items

--- spec/features/user_can_view_retired_items_spec.rb
+++ spec/features/user_can_view_retired_items_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "User can view retired items" do
-  skip
   scenario "they can visit retired items page" do
-    task = create(:task)
-    task.status = "retired"
+    city = create(:city_with_tasks)
+    task = city.tasks.last
+    task.update_attributes(status: "retired")
 
     visit task_path(task)
+    save_and_open_page
 
-    expect(page).to have_content("Item Retired")
+    expect(page).to have_content("Task Retired")
     expect(page).to_not have_content("Add to Cart")
   end
 end
