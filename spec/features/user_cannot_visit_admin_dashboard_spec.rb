9e6a5e0af09914d9bec67febf34a5f134910c81c
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-04-23 15:00:16 -0600

    Complete admin user 404 functionality

---
+++ spec/features/user_cannot_visit_admin_dashboard_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "User cannot view admin dashboard" do
+  scenario "visitor recieves a 404 error when visiting admin dashboard" do
+    visit "admin/dashboard"
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+  end
+
+  scenario "logged in user receives a 404 error when visiting admin dashboard" do
+    volunteer = create(:volunteer_with_tasks)
+    login_volunteer(volunteer)
+    visit "admin/dashboard"
+
+    expect(page).to have_content("The page you were looking for doesn't exist (404)")
+  end
+
+  def login_volunteer(volunteer)
+    visit login_path
+    fill_in "Username", with: volunteer.username
+    fill_in "Password", with: volunteer.password
+    click_button("LOGIN")
+  end
+end
