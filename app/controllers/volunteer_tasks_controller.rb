80128f280395179952f67fa21a5af5ce56d6fe1b
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-04-23 12:07:26 -0600

    Add checkout functionality and test

--- app/controllers/volunteer_tasks_controller.rb
+++ app/controllers/volunteer_tasks_controller.rb
@@ -5,8 +5,14 @@ class VolunteerTasksController < ApplicationController
   end
 
   def create
+    session[:cart].each do |k, v|
+      Task.find(k).update_attributes(volunteer_id: current_volunteer.id)
+    end
+    flash[:notice] = "Order was successfully placed"
+    session[:cart] = {}
+    redirect_to commitments_path
 
-                                  
+
     # checkout process
     # destroy cart session
 
