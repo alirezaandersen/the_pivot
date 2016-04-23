1011a3d5aee3adec5d25c3695a4b9b6d0a6ed37c
lingtran <lxiutran@gmail.com>
2016-04-23 15:01:54 -0600

    Refactor SessionsController with more redirect helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,7 @@ class SessionsController < ApplicationController
     if @volunteer && @volunteer.authenticate(params[:session][:password])
       session[:volunteer_id] = @volunteer.id
       flash[:notice] = "Logged in as #{@volunteer.username}"
-      if @volunteer.admin?
-        flash[:notice] = "You have been logged in as an admin"
-        redirect_to admin_dashboard_path
-      elsif session[:cart].nil?
-        redirect_to dashboard_path
-      else
-        redirect_to cart_path
-      end
-
+      role_redirect
     else
       flash.now[:error] = "Invalid. Please try again."
       render :new
@@ -28,4 +20,24 @@ class SessionsController < ApplicationController
     flash[:warning] = "Logged out!"
     redirect_to root_path
   end
+
+  private
+
+  def role_redirect
+    if @volunteer.admin?
+      flash[:notice] = "You have been logged in as an admin"
+      redirect_to admin_dashboard_path
+    else
+      cart_redirect
+    end
+  end
+
+  def cart_redirect
+    if session[:cart].nil?
+      redirect_to dashboard_path
+    else
+      redirect_to cart_path
+    end
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,6 @@ class Task < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
   validates :date, presence: true
-  # validates_format_of :date, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "^Date must be in the following format: mm/dd/yyyy"
   validates :start_time, presence: true
   validates :hours, presence: true
   validates :image_path, presence: true
