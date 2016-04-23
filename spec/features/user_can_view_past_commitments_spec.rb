3dce579471adc3a108304b7ec75b6b9a07fdee5e
Matt Pindell <pindell.m@gmail.com>
2016-04-22 18:27:55 -0600

    Add testing to cover: logged in users can only see their own commitments

--- spec/features/user_can_view_past_commitments_spec.rb
+++ spec/features/user_can_view_past_commitments_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.feature "User can view past commitments" do
   scenario "they can visit their past commitments page" do
     volunteer = create(:volunteer_with_tasks)
-    first_task = volunteer.tasks.first
-    last_task = volunteer.tasks.last
+    first_task, last_task = volunteer.tasks
 
     login_volunteer(volunteer)
 
@@ -13,11 +12,44 @@ RSpec.feature "User can view past commitments" do
     within(".commitment-table") do
       expect(page).to have_content(first_task.name)
       expect(page).to have_content(first_task.date)
+      expect(page).to have_content(first_task.display_time)
+      expect(page).to have_content(first_task.hours)
+      expect(page).to have_content(first_task.city_id)
 
       expect(page).to have_content(last_task.name)
       expect(page).to have_content(last_task.date)
+      expect(page).to have_content(last_task.display_time)
+      expect(page).to have_content(last_task.hours)
+      expect(page).to have_content(last_task.city_id)
     end
+  end
 
+  scenario "they can only see their own commitments" do
+    in_session_volunteer, not_in_session_volunteer = create_list(:volunteer_with_tasks, 2)
+
+    viewable_1, viewable_2 = in_session_volunteer.tasks
+    non_viewable_1, non_viewable_2 = not_in_session_volunteer.tasks
+
+    login_volunteer(in_session_volunteer)
+
+    visit commitments_path(in_session_volunteer)
+
+    within(".commitment-table") do
+      expect(page).to have_content(viewable_1.name)
+      expect(page).to have_content(viewable_1.date)
+      expect(page).to have_content(viewable_1.display_time)
+      expect(page).to have_content(viewable_1.hours)
+      expect(page).to have_content(viewable_1.city_id)
+
+      expect(page).to have_content(viewable_2.name)
+      expect(page).to have_content(viewable_2.date)
+      expect(page).to have_content(viewable_2.display_time)
+      expect(page).to have_content(viewable_2.hours)
+      expect(page).to have_content(viewable_2.city_id)
+
+      expect(page).to_not have_content(non_viewable_1.name)
+      expect(page).to_not have_content(non_viewable_2.name)
+    end
   end
 
   def login_volunteer(volunteer)
