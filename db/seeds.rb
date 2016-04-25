f984bdab6dd0dd3c8f9b260b7b10c988e4004672
Matt Pindell <pindell.m@gmail.com>
2016-04-25 13:13:11 -0600

    Add admin and restructure city/task assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,28 @@ city_one = City.create(name: "Denver",
 city_two = City.create(name: "Aurora",
                        state: "CO")
 
+Volunteer.create(first_name: "basic",
+                 last_name:  "user",
+                 username:   "user",
+                 password:   "password",
+                 email:      "fake_account@gmail.com")
+
+Volunteer.create(first_name: "super",
+                 last_name:  "user",
+                 username:   "admin",
+                 password:   "password",
+                 email:      "fake_account@yahoo.com",
+                 role:       1)
 
 1.upto(4) do |i|
+  Task.create(name:        "Name #{i + 4}",
+              description: "Description #{i}",
+              date:        "11/04/2016",
+              start_time:  Time.new,
+              hours:       "#{i}",
+              image_path:  "https://robohash.org/#{i}",
+              city_id:     city_two.id)
+
   Task.create(name:        "Name #{i}",
               description: "Description #{i}",
               date:        "11/04/2016",
@@ -13,14 +33,4 @@ city_two = City.create(name: "Aurora",
               hours:       "#{i}",
               image_path:  "https://robohash.org/#{i}",
               city_id:     city_one.id)
-end
-
-5.upto(9) do |i|
-  Task.create(name:        "Name #{i}",
-              description: "Description #{i}",
-              date:        "01/23/2017",
-              start_time:  Time.new,
-              hours:       "#{i}",
-              image_path:  "https://robohash.org/#{i}",
-              city_id:     city_two.id)
 end
