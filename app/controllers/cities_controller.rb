4e35e207b112d69ccca9fa71bdbc89c27cd7f6f7
Matt Pindell <pindell.m@gmail.com>
2016-04-27 18:19:34 -0600

    Add conditionals for task card buttons

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,6 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.includes(:tasks).find_by(slug: params[:name].parameterize)
-    flash[:notice] = "Tasks are coming soon to this city. The world is still your oyster." if @city.tasks.empty?
+    flash.now[:notice] = "Tasks are coming soon to this city. The world is still your oyster." if @city.tasks.empty?
   end
-
 end
