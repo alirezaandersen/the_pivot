b557c4be0eabe6cdbc34aa2ea154f98a26e4fd80
lingtran <lxiutran@gmail.com>
2016-05-11 00:39:11 -0600

    Pass Jobs controller test for GET index

---
+++ spec/controllers/jobs_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe JobsController, :type => :controller do
+  context "GET #index" do
+    it "assigns jobs as an instance variable" do
+      job = Job.create(title: "title", description: "description")
+      get :index
+      expect(assigns(:jobs)).to eq([job])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
