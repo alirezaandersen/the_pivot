69e851955d7736003a9a3168263eca09925e4440
lingtran <lxiutran@gmail.com>
2016-05-12 10:51:04 -0600

    Modify UsersJobsController

--- app/controllers/users_jobs_controller.rb
+++ app/controllers/users_jobs_controller.rb
@@ -1,19 +1,2 @@
-class CommitmentsController < ApplicationController
-
-  def show
-    if current_user.nil?
-      render file: '/public/404'
-    else
-      @upcoming_tasks  = current_user.tasks.pledged
-      @pending_tasks   = current_user.tasks.pending
-      @completed_tasks = current_user.tasks.completed
-    end
-  end
-
-  def create
-    Commitment.associate_tasks(session[:cart], current_user)
-    flash[:notice] = "You're Committed!"
-    session[:cart] = {}
-    redirect_to commitments_path
-  end
+class UsersJobsController < ApplicationController
 end
