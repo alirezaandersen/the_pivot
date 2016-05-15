eefe4fc3c7319c6a2be7d52a20726a94ee69731e
Alireza Andersen <alireza.andersen@gmail.com>
2016-05-15 11:28:13 -0600

    have bug that doesn't allow index html to view debugging stage part 1

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,6 +2,7 @@ class SubmissionsController < ApplicationController
 
   def index
     @submissions = Submission.all
+    # binding.pry
   end
 
   def new
@@ -22,46 +23,56 @@ class SubmissionsController < ApplicationController
 
   def show
     # binding.pry
+    # @submission = Submission.find(params[:id])
     @submission = Submission.find_by(company_name:params[:company_name])
+    binding.pry
   end
-
-  # def create
-  #   company_params = company_applications.except(:contact_us)
-  #   user_params = company_applications[:contact_us].merge({password_digest: 'asdjhgfasdf'}).except(:phone_number,:description)
-  #   binding.pry
-  #   @company = Company.new(company_params)
-  #   @company.users.create(user_params)
-  # end
 
   def approved_submissions
-    binding.pry
-    company_parameters
-    user_parameters
+      # binding.pry
+      @company = Company.create(company_info)
+      # binding.pry
+      @user = User.create(user_info.merge(company_id:@company[:id]))
+      role = Role.find_by(name:"store_admin")
+      UserRole.create(role_id:role[:id],user_id:@user[:id])
   end
 
-  def denied_submissions
+  def denied_submissions(params)
 
   end
 
-  def company_parameters
-    company_params[:name] = submissions_params[:company_name]
-    company_params[:description] = submissions_params[:about_company]
-    company_params[:logo] = submissions_params[:logo]
-    company_params[:url] = submissions_params[:url]
-    company_params[:size] = submissions_params[:size_of_company]
-    company_params[:industry] = submissions_params[:industry]
+  def submissions_parser
+    Submission.find_by(company_name: params[:company_name])
   end
 
-  def user_parameters
-    user_params[:first_name] = submissions_params[:first_name]
-    user_params[:last_name] = submissions_params[:last_name]
-    user_params[:email] = submissions_params[:email]
-    user_params[:password_digest] = submissions_params[:first_name]
+  def company_info
+    company_params = {}
+    company_params[:name] = submissions_parser.company_name
+    company_params[:description] = submissions_parser.about_company
+    company_params[:logo] = submissions_parser.logo
+    company_params[:url] = submissions_parser.url
+    company_params[:size] = submissions_parser.size_of_company
+    company_params[:industry] = submissions_parser.industry
+    company_params[:approve] = true
+    company_params
+  end
+
+  def user_info
+
+    user_params = {}
+    user_params[:first_name] = submissions_parser[:first_name]
+    user_params[:last_name] = submissions_parser[:last_name]
+    user_params[:email] = submissions_parser[:email]
+    user_params[:password_digest] = "password_digest"
+    user_params
   end
 
   private
 
   def submissions_params
-    params.require(:submission).permit(:company_name, :logo, :url, :size_of_company, :industry, :about_company, :first_name, :last_name, :email, :phone_number, :description)
+    #binding.pry
+    # params.require(:submission).permit(:company_name, :logo, :url, :size_of_company, :industry, :about_company, :first_name, :last_name, :email, :phone_number, :description)
+    params.permit(:company_name, :logo, :url, :size_of_company, :industry, :about_company, :first_name, :last_name, :email, :phone_number, :description)
+
   end
 end
