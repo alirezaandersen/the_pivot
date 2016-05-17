f07eb5ff0148ce0d78e45f2427fb56c9ed77873d
Alireza Andersen <alireza.andersen@gmail.com>
2016-05-17 11:39:26 -0600

    additional test in place for platform admin

---
+++ spec/features/platform_admin_can_create_jobs_for_specific_company_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+
+RSpec.feature "Platform Admin can create job current company clients" do
+  scenario "Platform Admin can see companies which are active" do
+
+    user = platform_admin_user
+    company = create(:company)
+     company.update(approve: true)
+
+    visit login_path
+
+    expect(page).to have_current_path login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_on "LOGIN"
+
+    expect(page).to have_current_path platform_admin_dashboard_path
+
+    click_link("Avaiable Companies")
+
+    expect(page).to have_current_path active_companies_path
+
+    expect(page).to have_content("Active Companies")
+    expect(page).to have_content("Total Active Companies")
+
+    click_on "CREATE JOB"
+
+    expect(page).to have_current_path create_company_jobs_path(company.id)
+
+    fill_in "Position Title", with: "Teacher"
+    fill_in "Department", with: "Education"
+    fill_in "Requirements", with: "Smiles"
+    fill_in "Job Type", with: "Full Time"
+    fill_in "Salary", with: "1000"
+    fill_in "Description of Job", with: "Programmer"
+
+    click_on "Create A Job"
+
+    expect(page).to have_current_path job_path("Teacher")
+    expect(page).to have_content("Teacher at #{company.name}")
+  end
+end
