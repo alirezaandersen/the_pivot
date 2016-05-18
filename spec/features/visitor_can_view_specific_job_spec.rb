d3701d6c531557b1778a8bf53091efdba67e2f5a
Alireza Andersen <alireza.andersen@gmail.com>
2016-05-17 19:40:32 -0600

    Resolve specific job view

--- spec/features/visitor_can_view_specific_job_spec.rb
+++ spec/features/visitor_can_view_specific_job_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.feature "Visitor can view a job page" do
   include ActionView::Helpers::NumberHelper
   scenario "they can see job details" do
-    city = create(:city_with_jobs)
-    job = city.jobs.first
+    job = create(:job)
     job.company.update(approve: true)
 
     visit jobs_path
@@ -20,7 +19,6 @@ RSpec.feature "Visitor can view a job page" do
     within("#job-text-box") do
       expect(page).to have_content job.company.name
       expect(page).to have_content job.title
-      expect(page).to have_content job.city.name_and_state
       expect(page).to have_content job.education
       expect(page).to have_content job.years_of_experience
       expect(page).to have_content job.department
