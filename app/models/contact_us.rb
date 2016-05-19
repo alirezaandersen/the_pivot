a43e01aaa1af67cdeb79e78ef6e6f02ac1b55222
lingtran <lxiutran@gmail.com>
2016-05-19 12:15:40 -0600

    Pass tests for validations for phone numbers in the contact_us and submission models

--- app/models/contact_us.rb
+++ app/models/contact_us.rb
@@ -6,6 +6,7 @@ class ContactUs < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, length: { is: 10, message: "Phone number %(value) can only be 10 digits" }
   validates :description, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 end
--- spec/models/contact_us_spec.rb
+++ spec/models/contact_us_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe ContactUs, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:phone_number) }
     it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.not_to allow_value(1234567).for(:phone_number) }
+    it { is_expected.to allow_value(1234567890).for(:phone_number) }
+    it { is_expected.not_to allow_value(12345678).for(:phone_number) }
+    it { is_expected.to allow_value("example@example.com").for(:email) }
+    it { is_expected.not_to allow_value("example@example").for(:email) }
   end
 end
